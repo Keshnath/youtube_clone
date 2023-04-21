5ce5d59d4729a9885d85aa5a8c7809683ddeac90
keshnath <princeshubham.a@gmail.com>
2023-04-21 15:59:36 +0530

    key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_KEY = "AIzaSyC0d_X1N44k2dDw5dkGjNQqqV97iZmIHYo";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+API_KEY ;
 
